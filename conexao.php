b06826c35182c02d474b523aa402f2724433464b
Lori-rls <lucaslealdes1@gmail.com>
2023-03-23 09:07:18 -0300

    faz a conexão, mas n mostra nada ainda, ele funciona tho \o/

--- conexao.php
+++ conexao.php
@@ -1,11 +1,14 @@
 <?php
-
+//Informações pra conexão
 $hostname = "localhost";
 $bancodedados = "bancodedados";
 $usuario = "root";
 $senha = "";
 
+//Instânciando objeto do tipo mysql e fazendo conexão
 $mysqli = new mysqli($hostname, $usuario, $senha, $bancodedados);
+
+//Caso erro:
 if ($mysqli->connect_errno) {
     echo "Falha ao conectar: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
